a3b7dca3a7adaedbf2e79fa2f0200567a5937f03
Rio <onwukachibike@gmail.com>
2023-02-02 14:43:42 +0100

    updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   solidity: "0.8.17",
   networks:{
     goerli: {
-      url: process.env.ALCHEMY_API_KEYy,
+      url: process.env.ALCHEMY_API_KEY,
       accounts: [process.env.PRIVATE_KEY],
     },
     mumbai:{
