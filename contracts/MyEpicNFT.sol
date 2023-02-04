c9c8fa03c79569ebe52486679e42860be3f29f92
Rio <onwukachibike@gmail.com>
2023-02-04 16:01:27 +0100

    updated

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -15,7 +15,7 @@ contract MyEpicNFT is ERC721URIStorage {
 
   string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
-  string[] firstWords = ["YOUR_WORD", "YOUR_WORD", "YOUR_WORD", "YOUR_WORD", "YOUR_WORD", "YOUR_WORD"];
+  string[] firstWords = ["Cryptosphere","VoidCraft","ShadowLand","Galaxia","Mysteria"];
   
 
   constructor() ERC721 ("RIONFT", "RIO") {
@@ -28,17 +28,7 @@ contract MyEpicNFT is ERC721URIStorage {
     return firstWords[rand];
   }
 
-  function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
-    uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
-    rand = rand % secondWords.length;
-    return secondWords[rand];
-  }
 
-  function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
-    uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
-    rand = rand % thirdWords.length;
-    return thirdWords[rand];
-  }
 
   function random(string memory input) internal pure returns (uint256) {
       return uint256(keccak256(abi.encodePacked(input)));
