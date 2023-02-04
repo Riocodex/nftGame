4b7b38574baf082872e6c445fbdc1c18f6641750
Rio <onwukachibike@gmail.com>
2023-02-04 16:00:41 +0100

    updated

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.0;
+pragma solidity 0.8.17;
 
 
 import "@openzeppelin/contracts/utils/Strings.sol";
@@ -16,10 +16,9 @@ contract MyEpicNFT is ERC721URIStorage {
   string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
   string[] firstWords = ["YOUR_WORD", "YOUR_WORD", "YOUR_WORD", "YOUR_WORD", "YOUR_WORD", "YOUR_WORD"];
-  string[] secondWords = ["YOUR_WORD", "YOUR_WORD", "YOUR_WORD", "YOUR_WORD", "YOUR_WORD", "YOUR_WORD"];
-  string[] thirdWords = ["YOUR_WORD", "YOUR_WORD", "YOUR_WORD", "YOUR_WORD", "YOUR_WORD", "YOUR_WORD"];
+  
 
-  constructor() ERC721 ("SquareNFT", "SQUARE") {
+  constructor() ERC721 ("RIONFT", "RIO") {
     console.log("This is my NFT contract. Woah!");
   }
 
