2ba209c39b724ead01ce994aa3c82d14cbb062f5
Rio <onwukachibike@gmail.com>
2023-02-05 22:10:41 +0100

    updated

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -17,6 +17,7 @@ contract MyEpicNFT is ERC721URIStorage {
   string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
   string[] firstWords = ["Cryptosphere","VoidCraft","ShadowLand","Galaxia","Mysteria"];
+  uint256 strenght = 1;
   
 
   constructor() ERC721 ("RIONFT", "RIO") {
