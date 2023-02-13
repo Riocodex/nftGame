605e04f22d693d6232c6f2c6b257e69f19aa27ec
Rio <onwukachibike@gmail.com>
2023-02-13 01:33:21 +0100

    updated

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -57,20 +57,18 @@ contract MyEpicNFT is ERC721URIStorage {
 
     // Get all the JSON metadata in place and base64 encode it.
     string memory json = Base64.encode(
-        bytes(
-            string(
-                abi.encodePacked(
-                    '{"name": "',
-                    // We set the title of our NFT as the generated word.
-                    combinedWord,
-                    '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',
-                    // We add data:image/svg+xml;base64 and then append our base64 encode our svg.
-                    Base64.encode(bytes(finalSvg)),
-                    '"}'
-                )
-            )
-        )
-    );
+      bytes(
+          string(
+              abi.encodePacked(
+                  '{'
+                      '"name": "', combinedWord, '", '
+                      '"description": "A highly acclaimed collection of squares.", '
+                      '"image": "data:image/svg+xml;base64,', Base64.encode(bytes(finalSvg)), '"'
+                  '}'
+              )
+          )
+      )
+  );
 
     // Just like before, we prepend data:application/json;base64, to our data.
     string memory finalTokenUri = string(
